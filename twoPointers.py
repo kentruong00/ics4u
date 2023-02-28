26be0a92b5e656b04518c908232dc74e64453af1
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2023-02-27 22:54:49 -0500

    Update twoPointers.py

--- twoPointers.py
+++ twoPointers.py
@@ -1,5 +1,23 @@
 def twoPointers(nums1, nums2):
-    output = []
-    for i, j in enumerate(nums1):
-        for a, b in enumerate(nums2):
-            
+    combined = []
+    i = 0
+    j = 0
+    while i < len(nums1) or j < len(nums2):
+        if i == len(nums1):
+            combined.append(nums2[j])
+            j += 1
+            continue
+        elif j == len(nums2):
+            combined.append(nums1[i])
+            i+=1
+            continue
+        if nums1[i] <= nums2[j]:
+            combined.append(nums1[i])
+            i += 1
+        else:
+            combined.append(nums2[j])
+            j += 1
+    return combined
+in1 = [1, 6, 6, 13, 18, 18]
+in2 = [2, 3, 8, 13, 15, 21, 25]
+print(twoPointers(in1, in2))
