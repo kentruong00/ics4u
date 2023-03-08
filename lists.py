880863a1f2020c8a899a69ce78742f113a7fbbfe
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2023-03-08 08:56:19 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -13,4 +13,41 @@ def swapAdj(word: str, target: int) -> str:
 print(swapAdj('hello', 1))
 def swap(word: str, num1: int, num2: int):
     #swap two latters of a string given 2 index location, num1 and num2
-    arr = 
+    # arr = 
+    return
+def mean(array: list) -> int:
+    #return the mean of a list 
+    if not array: # if len(arr) == 0, for other languages
+        return 0
+    for i in array:
+        total = sum(array)
+        return total/len(array)
+def median(array: list):
+    #return the median value of a list
+    if not arraySorted:
+        return 0
+    # array.sort() # modify/mutates the list, .sort() is a method that belongs to list
+    arraySorted = sorted(array) # returns a new list that is sorted
+    if len(arraySorted) % 2 == 0:
+        mid = len(arraySorted) / 2
+        return (arraySorted[mid]+arraySorted[mid-1])/2
+    else:
+        mid = len(arraySorted) // 2
+        return arraySorted[mid]
+def mode(array: list):
+    max = '0'
+    if not array:
+        return 0
+'''
+hi
+'''
+def sortReverse(word):
+    before = word
+    sorted = ''
+    while before:
+        largest = max(before)
+        sorted += largest
+        before.replace(largest, '')
+    return sorted
+print(sortReverse('hello'))
+
